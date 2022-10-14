864cb88050eeccf64ec2cf41e0db33eb4aa92af1
Kyryo <owenonions@gmail.com>
2022-10-14 16:44:40 +0200

    added a different implementation for responding to intents

--- chatBot/processWitIntents.py
+++ chatBot/processWitIntents.py
@@ -15,12 +15,19 @@ helpdesk_url = config["helpdesk_url"]
 # ********* Intents ************
 bot_null_response = intent_response["null"]
 bot_greeting_response = intent_response["greeting:greeting"]
-bot_greeting_log_intent = intent_response["greeting:greeting,intent_log_ticket:intent_log_ticket"]
-bot_greeting_check_intent = intent_response["greeting:greeting,intent_check_ticket:intent_check_ticket"]
+bot_greeting_log_intent = intent_response[
+    "greeting:greeting,intent_log_ticket:intent_log_ticket"
+]
+bot_greeting_check_intent = intent_response[
+    "greeting:greeting,intent_check_ticket:intent_check_ticket"
+]
 bot_greeting_intent_log_ticket = intent_response[
-    "greeting:greeting,intent_log_ticket:intent_log_ticket,log_ticket:log_ticket"]
-bot_greeting_intent_check_ticket = intent_response["greeting:greeting,intent_check_ticket:intention_check_ticket," \
-                                                   "check_ticket:check_ticket"]
+    "greeting:greeting,intent_log_ticket:intent_log_ticket,log_ticket:log_ticket"
+]
+bot_greeting_intent_check_ticket = intent_response[
+    "greeting:greeting,intent_check_ticket:intention_check_ticket,"
+    "check_ticket:check_ticket"
+]
 bot_greeting_log_ticket = "greeting:greeting,log_ticket:log_ticket"
 bot_greeting_check_ticket = "intent_log_ticket:intent_log_ticket,log_ticket:log_ticket"
 bot_intent_log_ticket = "intent_log_ticket:intent_log_ticket,log_ticket:log_ticket"
@@ -28,11 +35,42 @@ bot_intent_check_ticket = "intent_check_ticket:intent_check_ticket"
 bot_log_ticket = "log_ticket:log_ticket"
 bot_check_ticket = "check_ticket:check_ticket"
 
+# expected intents
+# for new intents, simple add them to this dict and everythin will still hold
+expected_intents = {
+    1: {
+        "intent": ["greeting:greeting"],
+        "response": "greeting:greeting",
+    },
+    2: {
+        "intent": ["intent_log_ticket:intent_log_ticket"],
+        "response": "intent_log_ticket:intent_log_ticket",
+    },
+    3: {
+        "intent": "[log_ticket:log_ticket]",
+        "response": "log_ticket:log_ticket",
+    },
+}
 
+
+def respond_to_intent(intents_from_nlp_engine, expected_intents, intent_response):
+
+    responses = intent_response
+
+    if not intents_from_nlp_engine:
+        return responses["null"]
+
+    for intent_from_nlp_engine in intents_from_nlp_engine:
+        for expected_intent in expected_intents:
+            if intent_from_nlp_engine == expected_intent["intent"]:
+                responses.append(expected_intent["response"])
+
+    return random.choice(responses)
 
 
 # **** Database operations *************
 # from utils.modules import conversation
+
 
 def process_intents(entity):
     """
@@ -45,7 +83,9 @@ def process_intents(entity):
     try:
 
         if entity == {}:
-            message_to_sender = random.choice(bot_null_response)  # get a random response
+            message_to_sender = random.choice(
+                bot_null_response
+            )  # get a random response
             return message_to_sender
         else:
 
@@ -60,31 +100,50 @@ def process_intents(entity):
 
                     if save_new_conversation:
                         # send the user a response message
-                        greetings_message_to_sender = random.choice(bot_greeting_response)  # get a random response
+                        greetings_message_to_sender = random.choice(
+                            bot_greeting_response
+                        )  # get a random response
                         return greetings_message_to_sender
 
-                if ["greeting:greeting", "intent_log_ticket:intent_log_ticket"] == entity:
+                if [
+                    "greeting:greeting",
+                    "intent_log_ticket:intent_log_ticket",
+                ] == entity:
                     save_new_conversation = True  # send to the database
                     if save_new_conversation:
                         message_to_client = random.choice(bot_greeting_log_intent)
                         return message_to_client
 
-                if ["greeting:greeting", "intent_check_ticket:intent_check_ticket"] == entity:
+                if [
+                    "greeting:greeting",
+                    "intent_check_ticket:intent_check_ticket",
+                ] == entity:
                     save_new_conversation = True
                     if save_new_conversation:
                         message_to_client = random.choice(bot_greeting_check_intent)
                         return message_to_client
-                if ["greeting:greeting", "intent_log_ticket:intent_log_ticket", "log_ticket:log_ticket"]:
+                if [
+                    "greeting:greeting",
+                    "intent_log_ticket:intent_log_ticket",
+                    "log_ticket:log_ticket",
+                ]:
                     save_new_conversation = True
                     if save_new_conversation:
-                        message_to_client = random.choice(bot_greeting_intent_log_ticket)
+                        message_to_client = random.choice(
+                            bot_greeting_intent_log_ticket
+                        )
                         return message_to_client
 
-                if ["greeting:greeting", "intent_check_ticket:intention_check_ticket", "check_ticket"
-                                                                                       ":check_ticket"]:
+                if [
+                    "greeting:greeting",
+                    "intent_check_ticket:intention_check_ticket",
+                    "check_ticket" ":check_ticket",
+                ]:
                     save_new_conversation = True
                     if save_new_conversation:
-                        message_to_client = random.choice(bot_greeting_intent_check_ticket)
+                        message_to_client = random.choice(
+                            bot_greeting_intent_check_ticket
+                        )
                         return message_to_client
                 if ["greeting:greeting", "log_ticket:log_ticket"]:
                     save_new_conversation = True
@@ -103,7 +162,10 @@ def process_intents(entity):
                     if save_new_conversation:
                         message_to_client = random.choice(bot_intent_log_ticket)
                         return message_to_client
-                if ["intent_check_ticket:intention_check_ticket", "check_ticket:check_ticket"]:
+                if [
+                    "intent_check_ticket:intention_check_ticket",
+                    "check_ticket:check_ticket",
+                ]:
                     save_new_conversation = True
                     if save_new_conversation:
                         message_to_client = random.choice(bot_intent_check_ticket)
@@ -125,7 +187,7 @@ def process_intents(entity):
                         return message_to_client
                 if ["check_ticket:check_ticket"]:
                     # that means the user has provided the ticket number and the conversation is now complete
-                    # the Ticket number must be sent back to Helpdesk 
+                    # the Ticket number must be sent back to Helpdesk
                     save_new_conversation = True
                     if save_new_conversation:
                         message_to_client = random.choice(bot_check_ticket)
@@ -136,6 +198,4 @@ def process_intents(entity):
 
     except:
         pass
-    return '200 OK HTTPS.'
-
-
+    return "200 OK HTTPS."
